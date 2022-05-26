83856885785f064e620b438fe5872dac93cc1d33
Josh Whetton <josh@whetton.net>
2022-05-25 21:33:40 -0400

    Basic functionality achieved.
    - Respond to newly opened issues with a comment
    - Set labels on newly opened issues such that support knows issues are ready for triage
    - Set labels based on last user to respond (owner: waiting on user, user: waiting on maintainer)
    
    TODO
    - Manage only our labels i.e. don't remove extra labels that exist on the issue

--- index.js
+++ index.js
@@ -6,10 +6,10 @@
 const config = require("./config.json");
 
 module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
+  app.log.info("Loaded!");
 
   app.on("issues.opened", async (context) => {
+    app.log.info("New issue. Posting first comment!");
     const issueComment = context.issue({
       body: config.messages.welcome,
     });
@@ -17,21 +17,30 @@ module.exports = (app) => {
   });
 
   app.on("issues.opened", async (context) => {
-    const newLabels = context.issue({labels:[config.labels.new]});
+    app.log.info("New issue. Adding labels!");
+    const newLabels = context.issue({
+      labels:[
+        config.labels.new,
+        config.labels.waitingOnYou
+      ]
+    });
     return context.octokit.issues.addLabels(newLabels);
   });
 
   app.on("issue_comment.created", async (context) => {
-    if (context.isBot) return;
-    const ourComment = context.issue({
-      body: config.messages.onComment,
-    });
-    return context.octokit.issues.createComment(ourComment);
+    if (context.isBot) return; // ignore the bots
+    app.log.info("New comment. Deciding which labels to apply!")
+    let waitingOn;
+    if (context.payload.comment.author_association === "OWNER") {
+      waitingOn = context.issue({
+        labels:[config.labels.waitingOnUser],
+      });
+    } else {
+      waitingOn = context.issue({
+        labels:[config.labels.waitingOnYou],
+      })
+    }
+    // completely overwrite labels for now...
+    return context.octokit.issues.setLabels(waitingOn);
   });
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
