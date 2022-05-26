251bcb973603225f9674d7358d84efeced4ffd16
Josh Whetton <josh@whetton.net>
2022-05-25 20:54:02 -0400

    Refactor to use a config.
    Move the configuration of labels and responses to a
    config file.

--- index.js
+++ index.js
@@ -2,26 +2,29 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
+
+const config = require("./config.json");
+
 module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: config.messages.welcome,
     });
     return context.octokit.issues.createComment(issueComment);
   });
 
   app.on("issues.opened", async (context) => {
-    const newLabels = context.issue({labels:["new"]});
+    const newLabels = context.issue({labels:[config.labels.new]});
     return context.octokit.issues.addLabels(newLabels);
   });
 
   app.on("issue_comment.created", async (context) => {
     if (context.isBot) return;
     const ourComment = context.issue({
-      body: "Hey! :wave:"
+      body: config.messages.onComment,
     });
     return context.octokit.issues.createComment(ourComment);
   });
