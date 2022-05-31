dcb488fdb44f1c6cc39ff27d6378248e6ebab62b
Josh Whetton <josh@whetton.net>
2022-05-31 13:44:36 -0400

    Refactor and bump version.
    Move some logic out into helper functions.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  * @param {import('probot').Probot} app
  */
 
-const botConfig = 'label-config.yml'
+const helpers = require("./lib/label-helpers");
+const botConfig = "label-config.yml";
 
 module.exports = (app) => {
   app.log.info("Loaded!");
@@ -31,44 +32,10 @@ module.exports = (app) => {
     let issueLabels = context.payload.issue.labels.map((label) => label.name);
     app.log.debug(`Current issue labels: ${issueLabels}`);
 
-    let waitingOn;
-    let toBeRemoved;
-    let removeNew;
-    if (context.payload.comment.author_association === "OWNER") {
-      waitingOn = config.labels.waitingOnUser;
-      toBeRemoved = config.labels.waitingOnYou;
-      removeNew = true;
-    } else {
-      waitingOn = config.labels.waitingOnYou;
-      toBeRemoved = config.labels.waitingOnUser;
-    }
+    let newLabels = helpers.pickLabels(config, context, issueLabels);
 
-    // remove the old label if it exists
-    if (issueLabels.includes(toBeRemoved)) {
-      app.log.debug(`Found label to remove: ${toBeRemoved}`);
-      delete issueLabels[issueLabels.indexOf(toBeRemoved)];
-    }
-
-    // make sure we remove the "new" label when it's
-    // no longer needed.
-    if (removeNew && issueLabels.includes(config.labels.new)) {
-      app.log.debug("This issue isn't 'new' anymore...");
-      delete issueLabels[issueLabels.indexOf(config.labels.new)];
-    }
-
-    // add the new label
-    issueLabels.push(waitingOn);
-
-    // clean the array
-    app.log.debug(`before clean: ${issueLabels}`);
-    issueLabels = issueLabels.filter((label) => {
-      return label !== null;
-    });
-    app.log.debug(`after clean: ${issueLabels}`);
-
-    // update issue labels
     return context.octokit.issues.setLabels(
-      context.issue({ labels: issueLabels })
+      context.issue({ labels: newLabels })
     );
   });
 };
